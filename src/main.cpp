01d1bed06185265f8e77274e1d1f2e05c5238248
Jacob Meyers <jakejem2010@gmail.com>
2026-02-05 21:43:38 -0500

    fix again

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@ int main(int argc, char* argv[]) {
 
     if (argc < 2) {
         std::cerr << "Alescript " << ale_version << "\nUsage: interpreter <program_file>\n";
-        std::cin;
         return 1;
     }
 
